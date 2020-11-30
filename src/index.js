686336a5f355b475b118ea15105626cd0f1d30ad
Freddy <freddyescobar@hotmail.fr>
2020-11-30 18:37:16 +0100

    perf: import only used bootstrap js modules

--- src/index.js
+++ src/index.js
@@ -7,6 +7,16 @@ import './styles/style.css'
 // import scripts
 import 'jquery-touchswipe'
 import './js/jqBootstrapValidation'
-import './js/bootstrap'
-import './js/blocs'
 import './js/formHandler'
+import './js/blocs'
+
+
+// import bootstrap components
+import 'bootstrap/js/alert'
+import 'bootstrap/js/button'
+import 'bootstrap/js/carousel'
+import 'bootstrap/js/collapse'
+import 'bootstrap/js/dropdown'
+import 'bootstrap/js/modal'
+import 'bootstrap/js/tooltip'
+import 'bootstrap/js/transition'
