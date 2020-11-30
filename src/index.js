ff7b9da4abd4cddfb8149c984698202161bce4bf
Freddy <freddyescobar@hotmail.fr>
2020-11-30 16:52:18 +0100

    fix: add missing index.js file to building the bundle

---
+++ src/index.js
@@ -0,0 +1,12 @@
+// import styles
+import './styles/bootstrap.scss'
+import './styles/et-line.css'
+import './styles/font-awesome.css'
+import './styles/style.css'
+
+// import scripts
+import 'jquery-touchswipe'
+import './js/jqBootstrapValidation'
+import './js/bootstrap'
+import './js/blocs'
+import './js/formHandler'
